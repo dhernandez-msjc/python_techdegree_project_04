3004bc41581680a2f690676e8b23df89d29cf671
David C Hernandez <58623347+dhernandez-msjc@users.noreply.github.com>
2022-07-30 17:15:11 -0700

    added documentation.

--- menu.py
+++ menu.py
@@ -2,15 +2,22 @@ import os
 
 
 class MenuItem:
-
+    """
+    Base class which stores a menu items description and has an
+    overridable execute function to execute the action of the item.
+    """
     def __init__(self, description: str) -> None:
         self.description = description
 
-    def execute(self):
+    def execute(self) -> None:
+        """
+        Abstract method to execute the items actions.
+        :return: None
+        """
         pass
 
 
-class ExitMenu(MenuItem):
+class QuitApplication(MenuItem):
 
     def execute(self):
         print('Thank you for shopping with us!')
@@ -18,11 +25,13 @@ class ExitMenu(MenuItem):
 
 
 class Menu:
-
+    """
+    Menu class runs a given menu.
+    """
     def __init__(self) -> None:
         self.menu_items = []
 
 
 if __name__ == '__main__':
-    menu_item = ExitMenu('Exit Menu')
+    menu_item = QuitApplication('Exit Menu')
     menu_item.execute()
