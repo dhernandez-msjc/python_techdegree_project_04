3cd22dbe8e5649f9d8f7f43540a8e3f8bad367c7
David C Hernandez <58623347+dhernandez-msjc@users.noreply.github.com>
2022-07-30 17:18:37 -0700

    adds Menu skeleton

--- menu.py
+++ menu.py
@@ -6,6 +6,7 @@ class MenuItem:
     Base class which stores a menu items description and has an
     overridable execute function to execute the action of the item.
     """
+
     def __init__(self, description: str) -> None:
         self.description = description
 
@@ -21,6 +22,7 @@ class QuitApplication(MenuItem):
     """
     Functor which provides an exit message when quitting application.
     """
+
     def execute(self):
         print('Thank you for shopping with us!')
         print('Closing the application.')
@@ -30,8 +32,21 @@ class Menu:
     """
     Menu class runs a given menu.
     """
+
     def __init__(self) -> None:
         self.menu_items = []
+
+    def add_menu_item(self) -> None:
+        pass
+
+    def display_menu(self) -> None:
+        pass
+
+    def run_selection(self) -> None:
+        pass
+
+    def run_menu(self) -> None:
+        pass
 
 
 if __name__ == '__main__':
