c00bd45801b10218fa82447846db98d4166b8ee1
David C Hernandez <58623347+dhernandez-msjc@users.noreply.github.com>
2022-08-01 03:54:12 -0700

    moves running of main menu to app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-def run_appliction() -> None:
-    pass
+from menu_functions import (build_main_menu)
+
+
+def run_application() -> None:
+    build_main_menu()
 
 
 if __name__ == '__main__':
-    run_appliction()
+    run_application()
