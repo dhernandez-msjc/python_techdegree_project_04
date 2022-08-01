eaa447c121f59ebd2559cd5fe7e378d1d1c08105
David C Hernandez <58623347+dhernandez-msjc@users.noreply.github.com>
2022-08-01 04:08:42 -0700

    adds inventory.db

---
+++ model.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine, Column, Integer, String, Date
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///inventory.db', echo=False)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+
+class Product(Base):
+    __tablename__ = 'inventory'
+    product_id = Column(Integer, primary_key=True)
+    product_name = Column(String)
+    product_quantity = Column(Integer)
+    product_price = Column(Integer)
+    date_updated = Column(Date)
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
