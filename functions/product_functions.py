7542b1b8f20eb93234b7317066f9dedb80526c6e
David C Hernandez <58623347+dhernandez-msjc@users.noreply.github.com>
2022-08-09 12:40:26 -0700

    corrected product expectations.

--- functions/product_functions.py
+++ functions/product_functions.py
@@ -82,22 +82,30 @@ class NewProduct(MenuItem):
     def execute(self) -> None:
         Menu.clear_console()
         product_name = input('Enter the product name: ')
+        product_price = _get_valid_integer(prompt='Enter the product price (ex. 9.75): ',
+                                           clean_function=clean.clean_price)
+        product_quantity = _get_valid_integer(prompt='Enter quantity of product: ',
+                                              clean_function=clean.clean_quantity)
+        date_updated = _get_valid_date()
+
+        # if the product doesn't exist, add the new product
         product_exists_in_db = session.query(Product).filter(Product.product_name == product_name).count() > 0
 
         if not product_exists_in_db:
-            product_price = _get_valid_integer(prompt='Enter the product price (ex. 9.75): ',
-                                               clean_function=clean.clean_price)
-            product_quantity = _get_valid_integer(prompt='Enter quantity of product: ',
-                                                  clean_function=clean.clean_quantity)
-            date_updated = _get_valid_date()
-
             new_product = Product(product_name=product_name, product_price=product_price,
                                   product_quantity=product_quantity, date_updated=date_updated)
             session.add(new_product)
-            session.commit()
             print('New product has been added.')
         else:
-            print('Product already exists in Database. Select udpate if you wish to update the product.')
+            # update the current DB product with the most updated date item
+            current_db_product = session.query(Product).filter(Product.product_name == product_name).first()
+
+            if date_updated > current_db_product.date_updated:
+                current_db_product.product_price = product_price
+                current_db_product.product_quantity = product_quantity
+                current_db_product.date_updated = date_updated
+
+        session.commit()
         time.sleep(2.0)
 
 
